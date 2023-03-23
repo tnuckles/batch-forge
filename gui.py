f36ad660fc2f97afb1ee4678a61b5a6eec9c6863
Trevor <trevnuckles@gmail.com>
2023-03-23 13:43:20 -0600

    Dependencies and existing code commit

---
+++ gui.py
@@ -0,0 +1,101 @@
+#!/Users/Trevor/Documents/Scripts/batch_forge python
+
+#UI Script for Batch Forge
+
+from tkinter import *
+from tkinter import ttk
+from wallpaperSorterFunctions import *
+import zipfile as zf
+import shutil
+
+installation_dir = '/Users/Trevor/Documents/Scripts/batch_forge/'
+try:
+    shutil.rmtree('/Volumes/Samsung SSD/caldera/var/public/3 Downloaded')
+    shutil.copytree('/Volumes/Samsung SSD/caldera/var/public/3 Downloaded Copy', '/Volumes/Samsung SSD/caldera/var/public/3 Downloaded')
+except:
+    pass
+
+sort_results = []
+
+root = Tk()
+app_image = Image('photo', file = installation_dir + 'assets/batch_forge_icon.png')
+root.call('wm', 'iconphoto',  root._w,  app_image)
+root.title('Batch Forge')
+root.geometry('300x350')
+root.minsize(300,350)
+root.maxsize(300,350)
+
+def sort_zipped_packages_window():
+    window = Toplevel(root)
+    window.title('Sort')
+    zippedPackages = sortPackagesByOrderNumber(glob.glob(gv.downloadDir + '*.zip'))
+    snort_label_count = len(zippedPackages)
+
+    if snort_label_count == 67:
+        snort_label = Label(window, text=f'Now sorting {snort_label_count} orders.\n\n'
+                            'ALL HAIL SIXTY SEVEN\nTHE 19TH AND HOLIEST PRIME')
+    else:
+        snort_label = Label(window, text=f'Now sorting {snort_label_count} orders.')
+    snort_label.pack(padx=10, pady=10)
+    progress_bar = ttk.Progressbar(window, orient='horizontal', length=300, mode='determinate')
+    progress_bar.pack(pady=10, padx=20)
+
+    status_label = Label(window, text='Working...')
+    status_label.pack()
+
+    progress_bar['maximum'] = 100
+
+    for package in zippedPackages:
+        status_label.config(text=package.split('/')[-1])
+        try:
+            fileToUnzipTo = gv.downloadDir + (package.split('/')[-1].split('_')[0]) + '/'
+            with zf.ZipFile(package, 'r') as zip_ref:
+                zip_ref.extractall(fileToUnzipTo)
+        except:
+                unzip_error_label = Label(f'| Couldn\'t unzip file: {package}')
+                unzip_error_label.pack(padx=5, pady=5)
+        orderJSON = str(glob.glob(fileToUnzipTo + '*.json')).split('\'')[1]
+        with open(orderJSON) as file:
+            openJSON = json.load(file)
+        parseJSON(openJSON, orderJSON, fileToUnzipTo)
+        splitMultiPagePDFs(glob.glob(fileToUnzipTo + '*.pdf'))
+        checkForMultiQtySamplePdfs(glob.glob(fileToUnzipTo + '*-Samp-*.pdf'))
+        try:
+            sortPdfsToSortedFolders(glob.glob(fileToUnzipTo + '*.pdf'))
+        except:
+            sort_results.append((f'| Couldn\'t properly sort PDFs in {fileToUnzipTo}'))
+
+        progress_bar['value'] += 100/snort_label_count
+        window.update_idletasks()
+
+    progress_bar['value'] = 100
+    status_label.config(text='Done!')
+
+    cleanupDownloadDir(gv.downloadDir)
+    if len(sort_results) == 0:
+        window.destroy()
+    else:
+        for result in sort_results:
+            result_label = Label(window, text=result)
+            result_label.pack(padx=2, pady=5)
+        close_button = Button(window, text='Return to Main Menu', command=window.destroy)
+        close_button.pack()
+
+main_menu_frame = LabelFrame(root, text='Main Menu', padx=10, pady=10)
+main_menu_frame.pack(padx=10, pady=10)
+
+button_sort_orders = Button(main_menu_frame, text='Sort Orders', width = 20, height = 2, command=sort_zipped_packages_window)
+button_batch_orders = Button(main_menu_frame, text='Build-A-Batch', width = 20, height = 2,)
+button_caldera_importer = Button(main_menu_frame, text='Caldera Importer', width = 20, height = 2,)
+button_drive_downloader = Button(main_menu_frame, text='Download from Drive', width = 20, height = 2,)
+button_dates_update = Button(main_menu_frame, text='Update Sort for Due Dates', width = 20, height = 2, command=moveForDueDates)
+button_quit = Button(main_menu_frame, text='Quit Batch Forge', width = 20, height = 2, command=root.quit)
+
+button_sort_orders.pack()
+button_batch_orders.pack()
+button_caldera_importer.pack()
+button_drive_downloader.pack()
+button_dates_update.pack()
+button_quit.pack()
+
+root.mainloop()
---
+++ wallpaperSorterFunctions.py
@@ -0,0 +1,384 @@
+#!usr/bin/env python
+
+import zipfile as zf
+import getPdfData as getPdf
+from batchCreate import tryToMovePDF
+import wallpaperSorterVariables as gv
+from macos_tags import get_all as checkTags
+from datetime import date, timedelta
+from add_macos_tag import apply_tag as applyTag
+from downloadFromDrive import transferFilesFromDrive
+import shutil, json, glob, pikepdf
+from os import remove, rmdir, walk, listdir, rename
+from re import findall
+
+today = date.today()
+missingPdfList = []
+damagedPdfList = []
+splitPdfList = []
+otPanelUknownList = []
+
+def startupChecks():
+    checkBatchCounter()
+    moveForDueDates(glob.glob(gv.sortingDir + '**/*.pdf', recursive=True))
+
+def checkBatchCounter():
+    if gv.globalBatchCounter['batchCounter'] > 9000:
+        gv.globalBatchCounter['batchCounter'] = 1
+
+def dueDateLookup(printPdf):
+    if 'order trouble' in str(checkTags(printPdf)):
+        return '1 - OT/'
+    else:
+        dueDate = getPdf.dueDate(printPdf)
+        if dueDate < date.today():
+            return '2 - Late/'
+        elif dueDate > date.today() + timedelta(days=1):
+            return '5 - Future/'
+        elif dueDate == date.today():
+            return '3 - Today/'
+        else:
+            return '4 - Tomorrow/'
+
+def moveForDueDates(pathToCheck):
+    print('\n| Updating Orders. Today\'s date:', today)
+    sortPdfsToSortedFolders(pathToCheck, True)
+    print('| Done updating orders based on Due Dates.')
+
+def sortPdfsToSortedFolders(pathToCheck, verbose=False):
+    for printPdf in pathToCheck:
+        friendlyName = getPdf.friendlyName(printPdf)
+        orderDueDate = dueDateLookup(printPdf)
+        material = getPdf.material(printPdf)
+        orderSize = getPdf.size(printPdf)
+        repeat = getPdf.repeat(printPdf)
+        oddOrEven = getPdf.oddOrEven(printPdf)   
+        orderLength = getPdf.length(printPdf)
+
+        # Checks if order is over the maximum length of a roll and moves it to Needs Attention
+        if '(OTPUnknown)' in printPdf.split('/')[-1]:
+            otPanelUknownList.append(getPdf.name(printPdf))
+            shutil.move(printPdf, gv.needsAttention + printPdf.split('/')[-1].split('.pdf')[0] + '_OT PANEL UNKNOWN.pdf')
+        if orderLength >= gv.dirLookupDict['MaterialLength'][gv.substrate[material]]:
+            tryToMovePDF(printPdf, gv.needsAttention, friendlyName, verbose)
+        else:
+            if orderSize == 'Samp':
+                newFilePath = gv.sortingDir + orderDueDate + gv.dirLookupDict[material] + 'Sample/' + printPdf.split('/')[-1]
+            else:
+                newFilePath = gv.sortingDir + orderDueDate + gv.dirLookupDict[material] + 'Full/' + gv.dirLookupDict['RepeatDict'][repeat] + gv.dirLookupDict[oddOrEven] + printPdf.split('/')[-1]
+        tryToMovePDF(printPdf, newFilePath, friendlyName)
+    
+    ordersInNeedsAttention = len(glob.glob(gv.needsAttention + '*.pdf'))
+    if ordersInNeedsAttention > 0:
+        print(f'\n| ****\n| 4 Needs Attention has {ordersInNeedsAttention} file(s) that need attention.\n| ****\n')
+
+def unzipRenameSortPdfs():
+    zippedPackages = sortPackagesByOrderNumber(glob.glob(gv.downloadDir + '*.zip'))
+    for package in zippedPackages:
+        try:
+            fileToUnzipTo = gv.downloadDir + (package.split('/')[-1].split('_')[0]) + '/'
+            with zf.ZipFile(package, 'r') as zip_ref:
+                zip_ref.extractall(fileToUnzipTo)
+        except:
+                print('| Couldn\'t unzip file:', package)
+        orderJSON = str(glob.glob(fileToUnzipTo + '*.json')).split('\'')[1]
+        with open(orderJSON) as file:
+            openJSON = json.load(file)
+        parseJSON(openJSON, orderJSON, fileToUnzipTo)
+        splitMultiPagePDFs(glob.glob(fileToUnzipTo + '*.pdf'))
+        checkForMultiQtySamplePdfs(glob.glob(fileToUnzipTo + '*-Samp-*.pdf'))
+        try:
+            sortPdfsToSortedFolders(glob.glob(fileToUnzipTo + '*.pdf'))
+        except:
+            print(f'| Couldn\'t properly sort PDFs in {fileToUnzipTo}')
+
+    reportListOfPdfs(splitPdfList, 'were split into multiple files and properly sorted')
+    reportListOfPdfs(missingPdfList, 'were missing and were moved to 4 Needs Attention')
+    reportListOfPdfs(damagedPdfList, 'were damaged and were moved to 4 Needs Attention')
+    reportListOfPdfs(otPanelUknownList, 'had OT panels that couldn\'t be read and were moved to 4 Needs Attention')
+
+    cleanupDownloadDir(gv.downloadDir)
+
+    transferFilesFromDrive()
+
+
+def parseJSON(openFile, JSONPath, fileToUnzipTo):
+    count = 1
+    JSONitem = openFile['order']['item']
+
+    if isinstance(JSONitem, list):
+        for itemNum in range(len(JSONitem)):
+            count = renamePdfWithDetails(openFile, JSONitem[itemNum], JSONPath, fileToUnzipTo, count)
+    else:
+        count = renamePdfWithDetails(openFile, JSONitem, JSONPath, fileToUnzipTo, count)
+
+    return
+
+def renamePdfWithDetails(openFile, JSONitem, JSONPath, fileToUnzipTo, count):
+    orderNumber = openFile['orderNumber']
+    keepTrackOfOrderNumber(orderNumber)
+    orderTroubleStatus = openFile['type']
+    orderDueDate = openFile['orderDueDate']
+    shipVia = gv.shippingMethods[openFile['shippingInfo']['method']['shipvia']]
+    originalPDFName = JSONitem['filename']
+    orderItemID = originalPDFName.split('_')[0]
+    itemID = originalPDFName.split('_')[0]
+    try:
+        templateName = JSONitem['description'].split(' ')[2] + ' ' + JSONitem['description'].split(' ')[3]
+    except IndexError:
+        templateName = JSONitem['description'].split(' Wallpaper')[0]
+    paperType = gv.substrate[JSONitem['paper']]
+    quantity = JSONitem['quantityOrdered']
+    height = JSONitem['height'] 
+    width = JSONitem['width']
+    repeat = JSONitem['wallpaperRepeat']
+    otNotes = openFile['order_trouble_notes']
+    if len(openFile['order_trouble_notes']) != 0:
+        orderTroubleNotes = parseOTNotes(otNotes, orderItemID, repeat)
+        if orderTroubleNotes != False:
+            templateName = templateName + ' ' + orderTroubleNotes
+
+
+    if width == '9':
+        orderSize = 'Samp'
+        length = '9.5'
+        height = '9'
+        width = '25'
+    else:
+        width = str(int(width) * int(quantity) + 1)
+        height = str(int(height) + 4.25)
+        if height == '148.25':
+            height = '146.25'
+        orderSize = 'Full'
+        length = str(getPdf.calculateLength(quantity, height))
+        # See Length Notes at the end of the function for an explanation.
+    newPDFName = orderNumber + '-' + str(count) + '-(' + orderDueDate + ')-' + shipVia + '-' + paperType + '-' + orderSize + '-Rp ' + repeat.split("'")[0] + '-Qty ' + quantity + '-' + templateName + '-L' + length + '-W' + width + '-H' + height
+    renamePDF(originalPDFName, newPDFName, JSONPath)
+    if orderTroubleStatus != 'new':
+        applyTag('order trouble', gv.downloadDir +  '/' + getPdf.orderNumber(newPDFName) + '/' + newPDFName + '.pdf')
+    keepTrackOfPDF(orderNumber, originalPDFName) 
+    count += 1
+    if orderNumber in gv.orderItemsDict:
+        gv.orderItemsDict[orderNumber][itemID] = {
+            'Status': 'Sorted',
+            'Due Date': orderDueDate,
+            'Shipping': shipVia,
+            'Material': paperType,
+            'Order Size': orderSize,
+            'Repeat': repeat.split('\'')[0],
+            'Quantity': quantity,
+            'Template': templateName,
+            'Length': length,
+            'Width': width,
+            'Height': height,
+            'OT Notes': otNotes,
+            'File Path': gv.sortingDir + '2 - Late/' + gv.dirLookupDict[paperType] + gv.dirLookupDict[orderSize] + gv.dirLookupDict['RepeatDict'][int(repeat.split('\'')[0])] + gv.dirLookupDict[int(quantity) % 2] + newPDFName,
+        }
+    else:
+        gv.orderItemsDict[orderNumber] = {
+            itemID : {
+                'Status': 'Sorted',
+                'Due Date': orderDueDate,
+                'Shipping': shipVia,
+                'Material': paperType,
+                'Order Size': orderSize,
+                'Repeat': repeat.split('\'')[0],
+                'Quantity': quantity,
+                'Template': templateName,
+                'Length': length,
+                'Width': width,
+                'Height': height,
+                'OT Notes': otNotes,
+                'File Path': gv.sortingDir + '2 - Late/' + gv.dirLookupDict[paperType] + gv.dirLookupDict[orderSize] + gv.dirLookupDict['RepeatDict'][int(repeat.split('\'')[0])] + gv.dirLookupDict[int(quantity) % 2] + newPDFName,
+            }
+        }
+
+    ''' # Length Notes: I will never remember why I did this, so here are my notes. The length is the length of material an order will take up.
+        # Length Notes: The above equation takes the quantity and divides it by two since we can fit two panels side by side.
+        # Length Notes: it then multiplies that by the height to get the overall length of material.
+        # Length Notes: after, it takes the quantity/2, rounds it down, and multiplies it by .5 for the .5" gap between each panel.
+        # Length Notes: lastly, it takes the quantity % 2 to see if the quantity is odd or not. If the quantity is odd, then it will add on one more length of .5"
+        # Length Notes: for the times that a panel is by itself and still has another .5" section.'''
+    
+    return count
+
+def parseOTNotes(otNotes, orderItemID, repeat):
+    if "'" in repeat:
+        repeat = repeat.split("'")[0]
+    try:
+        orderItemIDFromUser = str(findall(r"(?<!\d)\d{7}(?!\d)", otNotes)) #should find any 7 digit number for the orderItemID
+        if orderItemIDFromUser != '[]':
+            orderItemIDFromUser = orderItemIDFromUser.split("['")[1].split("']")[0]
+            if orderItemIDFromUser != orderItemID:
+                return False
+        panelID = str(findall(r"\D+(\d+)", otNotes)) #should find a digit after a string
+        if panelID != '[]':
+            panelID = panelID.split("['")[1].split("']")[0] 
+            if int(panelID) > (int(repeat)/2):
+                formattedOTNotes = '(OTPUnknown)'
+                return formattedOTNotes
+        formattedOTNotes = '(OTP' + panelID + ')'
+        if formattedOTNotes == '(OTP[])':
+            formattedOTNotes = False
+
+        return formattedOTNotes
+    except:
+        formattedOTNotes = '(OTPUnknown)'
+        return formattedOTNotes
+
+def renamePDF(old, new, JSONFilePath):
+    extension = old.split(".")[-1]
+    itemName = new + "." + extension
+    dirPath = JSONFilePath.split(JSONFilePath.split('/')[-1])[0]
+    try:
+        shutil.copy(dirPath + old,dirPath + old + ' - temp.pdf')
+        rename(dirPath + old, dirPath + itemName)
+        remove(dirPath + old + ' - temp.pdf')
+    except OSError:
+        try:
+            shutil.move(JSONFilePath, gv.needsAttention)
+        except:
+            pass
+        missingPdfList.append(itemName.split("-")[0])
+    return
+
+def keepTrackOfPDF(orderNumber, pdfFileName):
+    if pdfFileName in gv.countOfRefPdfs[orderNumber]:
+        gv.countOfRefPdfs[orderNumber][pdfFileName] += 1
+    else:
+        gv.countOfRefPdfs[orderNumber][pdfFileName] = 1
+
+def keepTrackOfOrderNumber(orderNumber): #keeps track of the original PDF names to alert fulfillment to multi-paper type PDFs
+    if orderNumber in gv.countOfRefPdfs:
+        pass
+    else:
+        gv.countOfRefPdfs[orderNumber] = {}
+
+def reportDuplicatePDFs(): #prints out any PDFs from keepTrackOfOrderNumber() that have a count of 2 or more
+    for orderNumber in gv.countOfRefPdfs:
+        for pdfName in gv.countOfRefPdfs[orderNumber]:
+            if gv.countOfRefPdfs[orderNumber][pdfName] > 1:
+                print('\n| The following Order has samples with multiple material types:')
+                print('| ' + orderNumber + ': ' + pdfName.replace('_',' '))
+
+def removeUnneededUnzippedFiles(pathToCheck):
+    for file in pathToCheck:
+        try:
+            remove(file)
+        except:
+            print(f'| Couldn\'t remove {file}')
+
+def removeEmptyDirectories(batchDirectory): # once a batch folder has been made, walks through the batch folder and removes any empty directories.
+    dirToWalk = list(walk(batchDirectory))
+    for path, _, _ in dirToWalk[::-1]:
+        if len(listdir(path)) == 0:
+            rmdir(path)
+    return
+
+def cleanupDownloadDir(pathToClean):
+    try:
+        removeUnneededUnzippedFiles(glob.glob(pathToClean + '*/*.xml'))
+        try:
+            removeUnneededUnzippedFiles(glob.glob(pathToClean + '*/*.json'))
+            try:
+                removeEmptyDirectories(pathToClean)
+                try:
+                    removeUnneededUnzippedFiles(glob.glob(pathToClean + '*.zip'))
+                except:
+                    print('| Unknown error while removing zip folders in 3 Downloaded')
+                    pass
+            except:
+                print('| Unknown error while removing empty folders in 3 Downloaded')
+                pass
+        except:
+            print('| Unknown error while removing JSON files in 3 Downloaded')
+            pass
+    except:
+        print('| Unknown error while removing XML files in 3 Downloaded')
+        pass
+    return
+
+def splitMultiPagePDFs(pathGlobToCheck):
+    results = []
+    for file in pathGlobToCheck:
+        try:
+            pdf = pikepdf.Pdf.open(file)
+            NumOfPages = len(pdf.pages)
+        except:
+            damagedPdfList.append(getPdf.name(file))
+            shutil.move(file, gv.needsAttention + file.split('/')[-1].split('.pdf')[0] + '_DAMAGED.pdf')
+            pass
+        try:
+            NumOfPages = len(pdf.pages)
+            if NumOfPages > 1:
+                templateName = getPdf.templateName(file)
+                namePt1 = file.split('Qty ')[0] + 'Qty '
+                namePt2 = file.split(templateName)[1]
+                quantity = getPdf.quantity(file)
+                for n, page in enumerate(pdf.pages):
+                    dst = pikepdf.Pdf.new()
+                    dst.pages.append(page)
+                    dst.save(namePt1 + str(quantity) + '-' + templateName + ' Panel ' + str(n + 1) + namePt2)
+                try:
+                    remove(file)
+                    splitPdfList.append(getPdf.friendlyName(file))
+                except:
+                    results.append(f'| Split the pages of {file},\nbut couldn\'t remove the original.')
+        except:
+            pass
+    return results
+
+def checkForMultiQtySamplePdfs(pdfList):
+    listOfSampsToDuplicate = []
+    
+    for printPdf in pdfList:
+        quantity = getPdf.quantity(printPdf)
+        if quantity > 1:
+            pdfName = getPdf.templateName(printPdf)
+            sampToDuplicate = []
+            sampToDuplicate.append(printPdf)
+            for i in range(quantity):
+                firstHalf = printPdf.split('Qty ' + str(quantity))[0]
+                pdfNameWithCounter = pdfName + '(' + str(i+1) + ')'
+                secondHalf = printPdf.split(pdfName)[1]
+                newNameToDuplicate = firstHalf + 'Qty 1-' + pdfNameWithCounter +  secondHalf
+                sampToDuplicate.append(newNameToDuplicate)
+            listOfSampsToDuplicate.append(tuple(sampToDuplicate))
+    
+    for tupleOfSamples in listOfSampsToDuplicate:
+        sampToDuplicateFrom = tupleOfSamples[0]
+        for i in tupleOfSamples:
+            if i == sampToDuplicateFrom:
+                continue
+            else:
+                shutil.copy(sampToDuplicateFrom, i)
+        remove(sampToDuplicateFrom)
+
+    return
+
+def reportListOfPdfs(list_to_report, str_to_print):
+    results = []
+    if len(list_to_report) > 0:
+        for item in list_to_report:
+            if item == None:
+                pass
+            else:
+                results.append(f'{item} {str_to_print}')
+        return results
+    else:
+        return
+
+def sortPackagesByOrderNumber(packageList): # takes a list of pathstopdfs and sorts them by orderNumber, from least to greatest.
+    listToSort = []
+    sortedList = []
+    for package in packageList:
+        pdfOrderNumber = package.split('/')[-1].split('_')[0]
+        listToSort.append((pdfOrderNumber, package))
+    listToSort.sort(reverse=False, key=lambda pdf: pdf[0])
+    packageList = listToSort
+    listToSort = []
+    for package in packageList:
+        sortedList.append(package[1])
+    packageList = sortedList
+    sortedList = []
+    return packageList
