d3549e21da21485b5b6b1e9a1348e769363d4f2b
Trevor <trevnuckles@gmail.com>
2023-04-18 16:28:04 -0600

    Style Guide Updates
    get_pdf_data.py

--- get_pdf_data.py
+++ get_pdf_data.py
@@ -7,21 +7,21 @@ from datetime import datetime
 def name(pdf):
     return pdf.split('/')[-1].split('.pdf')[0]
 
-def friendlyName(pdf):
-    friendlyPdfName = orderNumber(pdf) + ' ' +  orderItem(pdf) + ' ' + templateName(pdf)
-    return friendlyPdfName
+def friendly_name(pdf):
+    friendly_pdf_name = order_number(pdf) + ' ' +  order_item(pdf) + ' ' + template_name(pdf)
+    return friendly_pdf_name
 
-def orderNumber(pdf):
+def order_number(pdf):
     return name(pdf).split('-')[0]
 
-def orderItem(pdf):
+def order_item(pdf):
     return name(pdf).split('-')[1]
 
-def dueDate(pdf):
-    fileName = name(pdf)
-    return datetime.date(datetime.strptime(fileName.split('(')[1].split(')')[0], '%Y-%m-%d')) 
+def due_date(pdf):
+    file_name = name(pdf)
+    return datetime.date(datetime.strptime(file_name.split('(')[1].split(')')[0], '%Y-%m-%d')) 
 
-def shipMethod(pdf):
+def ship_method(pdf):
     return name(pdf).split('-')[5]
 
 def material(pdf):
@@ -43,10 +43,10 @@ def repeat(pdf):
 def quantity(pdf):
     return int(name(pdf).split('-')[9].split('Qty ')[1])
 
-def oddOrEven(pdf):
+def odd_or_even(pdf):
     return int(name(pdf).split('-')[9].split('Qty ')[1]) % 2
 
-def templateName(pdf):
+def template_name(pdf):
     return name(pdf).split('-')[10]
 
 def length(pdf):
@@ -58,7 +58,7 @@ def width(pdf):
 def height(pdf):
     return float(name(pdf).split('-')[13].split('H')[1])
 
-def calculateLength(quantity, height):
+def calculate_length(quantity, height):
     quantity = int(quantity)
     height = float(height)
 
@@ -68,19 +68,19 @@ def calculateLength(quantity, height):
     length = second * third
     return length
 
-def getAll(pdf):
-    pdfDict = {
-        'orderNumber': orderNumber(pdf),
-        'orderItem': orderItem(pdf),
-        'dueDate': dueDate(pdf),
-        'shipMethod': shipMethod(pdf),
+def get_all(pdf) -> dict:
+    pdf_dict = {
+        'order_number': order_number(pdf),
+        'order_item': order_item(pdf),
+        'due_date': due_date(pdf),
+        'ship_method': ship_method(pdf),
         'material': material(pdf),
-        'orderSize': size(pdf),
-        'orderRepeat': repeat(pdf),
-        'orderQuantity': quantity(pdf),
-        'templateName': templateName(pdf),
-        'orderLength': length(pdf),
-        'orderWidth': width(pdf),
-        'orderHeight': height(pdf),
+        'order_size': size(pdf),
+        'order_repeat': repeat(pdf),
+        'order_quantity': quantity(pdf),
+        'template_name': template_name(pdf),
+        'order_length': length(pdf),
+        'order_width': width(pdf),
+        'order_height': height(pdf),
     }
-    return pdfDict
+    return pdf_dict
